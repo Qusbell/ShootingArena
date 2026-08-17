5611723dcea38a936efb3a151eed6cd266560b38
Qusbell <01024072245a@gmail.com>
2026-08-17 17:28:24 +0900

    에디터 추가

---
+++ Source/ShootingArenaEditorTools/Private/ShootingArenaEditorTools.Build.cs
@@ -0,0 +1,20 @@
+﻿using UnrealBuildTool;
+
+public class ShootingArenaEditorTools : ModuleRules
+{
+    public ShootingArenaEditorTools( ReadOnlyTargetRules Target ) : base( Target )
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "AssetRegistry",
+                "UnrealEd"
+            }
+        );
+    }
+}
